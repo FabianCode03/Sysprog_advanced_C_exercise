68d77651f04af7232ceb130733d4d313f0beb5c1
FabianCode03 <behrendtf5@gmail.com>
2023-10-20 01:43:57 +0200

    Implement n,l cases in main switch, infrastucture
    -add static helper function to main.c
    -implement the list functios and add static helper functions in list.c
    -correct typos and spacing in list.h
    -add a Makefile

--- list.h
+++ list.h
@@ -4,10 +4,9 @@ typedef struct ListNode
     char number[32];
     struct ListNode *prev;
     struct ListNode *next;
-}ListNode;
+} ListNode;
 
-
-ListNode * listAdd(const char *name);
+ListNode *listAdd(const char *name);
 /*Hängt einen neuen Eintrag mit dem übergebenen name
 an das Ende der Liste. Die Telefonnummer wird auf den
 leeren String initialisiert. Gibt einen Zeiger auf den neu-
@@ -16,7 +15,6 @@ einem Fehler wird zudem errno auf einen entsprechen-
 den Wert gesetzt: EEXIST, falls der schon ein Eintrag mit
 demübergebenennameexistiert;oderENOMEM,fallsnicht
 genügend Speicher zur Verfügung steht.*/
-
 
 void listForEach(void (*func)(ListNode *));
 /*Iteriert über die komplette Liste und führt dabei die per
@@ -28,14 +26,12 @@ dadurch nAufrufe von func. Durch diesen Mechanismus
 können beispielsweise alle Elemente der Liste auf der
 Konsole ausgegeben werden.*/
 
-
 int listRemoveByName(const char *name);
 /*Löscht den Eintrag mit dem übergebenen name aus der
 Liste und gibt den Speicher für den Eintrag frei. Falls es
 keinen Eintrag mit dem übergebenen Namen gibt, wird
 errno auf ENOENT gesetztund-1 zurückgegeben.ImEr-
 folgsfall ist der Rückgabewert 0.*/
-
 
 void listRemoveAll(void);
 /*Löscht alle Einträge aus der Liste. Diese Funktion wird
